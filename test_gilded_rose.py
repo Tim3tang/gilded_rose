9595254e6b1e813ea2fa92b07671848b8d201e98
Tim3tang <Tim3tang@gmail.com>
2016-10-19 13:01:06 +0100

    added test update_quality_for_normal_items passing with pytest

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
+import unittest
 import pytest
 
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_update_quality_for_normal_items(self):
+        items = [Item("foo", 1, 1)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals(items[0].quality, 0)
 
 if __name__ == '__main__':
     unittest.main()
